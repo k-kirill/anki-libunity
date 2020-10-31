244520ec912f01b86eb878d7859a635c16bd2acf
Kirill Kirillov <kirill.kirillov@yahoo.com>
2020-10-31 19:41:13 +0300

    - count new cards - refresh on showing new questions

--- anki_libunity/__init__.py
+++ anki_libunity/__init__.py
@@ -1,12 +1,16 @@
 from aqt import mw
 from aqt import gui_hooks
-from anki import hooks
 from PyQt5 import QtDBus, QtCore
 
-def setProgress():
-    dueCardCount = len(mw.col.find_cards("is:due"))
-    studiedCardCount = len(mw.col.find_cards("rated:1"))
-    progress = studiedCardCount / (studiedCardCount + dueCardCount)
+
+def callback(card):
+    set_progress()
+
+
+def set_progress():
+    due_card_count = len(mw.col.find_cards("is:new or is:due"))
+    studied_card_count = len(mw.col.find_cards("rated:1"))
+    progress = studied_card_count / (studied_card_count + due_card_count)
 
     interface = "com.canonical.Unity.LauncherEntry"
     bus = QtDBus.QDBusConnection.sessionBus()
@@ -14,10 +18,12 @@ def setProgress():
         print("Not connected to dbus!")
     signal = QtDBus.QDBusMessage.createSignal("/", interface, "Update")
     signal << "application://net.ankiweb.Anki.desktop"
-    qtDueCardCount = QtCore.QVariant(dueCardCount)
-    qtDueCardCount.convert(QtCore.QVariant.LongLong)
-    signal << {'count': qtDueCardCount, 'count-visible': True, 'progress': progress, 'progress-visible': True}
+    qt_due_card_count = QtCore.QVariant(due_card_count)
+    qt_due_card_count.convert(QtCore.QVariant.LongLong)
+    signal << {'count': qt_due_card_count, 'count-visible': True, 'progress': progress, 'progress-visible': True}
     bus.send(signal)
 
-gui_hooks.main_window_did_init.append(setProgress)
-gui_hooks.reviewer_will_end.append(setProgress)
+
+gui_hooks.main_window_did_init.append(set_progress)
+gui_hooks.reviewer_will_end.append(set_progress)
+gui_hooks.reviewer_did_show_question.append(callback)
