ca85e342e3bb7b679f640533bf0eab2f2129e26b
Kirill Kirillov <kirill.kirillov@yahoo.com>
2021-01-06 22:15:24 +0300

    Fixes the error in #4
    For new collections the number of new and due cards can be zero,
    hence to avoid division by zero the progress is set to 100%.

--- anki_libunity/__init__.py
+++ anki_libunity/__init__.py
@@ -6,8 +6,11 @@ from PyQt5 import QtDBus, QtCore
 def set_progress():
     due_card_count = len(mw.col.find_cards("is:new or is:due"))
     studied_card_count = len(mw.col.find_cards("rated:1"))
-    progress = studied_card_count / (studied_card_count + due_card_count)
-
+    divisor = studied_card_count + due_card_count
+    if divisor == 0:
+        progress = 1
+    else:
+        progress = studied_card_count / divisor
     interface = "com.canonical.Unity.LauncherEntry"
     bus = QtDBus.QDBusConnection.sessionBus()
     if not bus.isConnected():
@@ -24,4 +27,3 @@ gui_hooks.collection_did_load.append(lambda _: set_progress())
 gui_hooks.reviewer_did_show_question.append(lambda _: set_progress())
 gui_hooks.reviewer_will_end.append(set_progress)
 gui_hooks.sync_did_finish.append(set_progress)
-
