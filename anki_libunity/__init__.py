bb924e5aa7432ce2ca7f43cce7b66017c9f68da3
Kirill Kirillov <kirill.kirillov@yahoo.com>
2021-01-16 17:42:43 +0300

    Hopefully, it fixes #4 as now the desktop filename is configurable.

--- anki_libunity/__init__.py
+++ anki_libunity/__init__.py
@@ -11,18 +11,22 @@ def set_progress():
         progress = 1
     else:
         progress = studied_card_count / divisor
+    signal = "Update"
+    path = "/"
     interface = "com.canonical.Unity.LauncherEntry"
     bus = QtDBus.QDBusConnection.sessionBus()
     if not bus.isConnected():
         print("Not connected to dbus!")
-    signal = QtDBus.QDBusMessage.createSignal("/", interface, "Update")
-    signal << "application://net.ankiweb.Anki.desktop"
+    signal = QtDBus.QDBusMessage.createSignal(path, interface, signal)
+    signal << "application://" + app_name + ".desktop"
     qt_due_card_count = QtCore.QVariant(due_card_count)
     qt_due_card_count.convert(QtCore.QVariant.LongLong)
     signal << {'count': qt_due_card_count, 'count-visible': True, 'progress': progress, 'progress-visible': True}
     bus.send(signal)
 
 
+config = mw.addonManager.getConfig(__name__)
+app_name = config['app_name']
 gui_hooks.collection_did_load.append(lambda _: set_progress())
 gui_hooks.reviewer_did_show_question.append(lambda _: set_progress())
 gui_hooks.reviewer_will_end.append(set_progress)
