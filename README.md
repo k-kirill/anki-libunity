8ec19238a350a51cf7fd1e3be8b0fe50eb656c46
Kirill Kirillov <kirill.kirillov@yahoo.com>
2021-01-24 19:37:07 +0300

    Adds the support of D-Bus desktop notifications

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 
 It works only on Linux with the panels/docks that support libunity such as KDE Plasma or Dash to Dock extension for GNOME.
 
-![](screenshot.png)
+![](screenshot_launcher.png)
+
+Also, it displays notifications when the number of cards to study increases. For example, when there are new due cards.
+
+![](screenshot_notification.png)
 
 ## Install
 Please check the official add-on web page: https://ankiweb.net/shared/info/901237400
+
+## Configuration
+If your Anki's desktop file is not named `net.ankiweb.Anki.desktop`, then please change the application name in the add-on's config. It is used to find the Anki launcher and the Anki icon (for notifications).
--- anki_libunity/__init__.py
+++ anki_libunity/__init__.py
@@ -4,6 +4,7 @@ from PyQt5 import QtDBus, QtCore
 
 
 def set_progress():
+    global app_name
     due_card_count = len(mw.col.find_cards("is:new or is:due"))
     studied_card_count = len(mw.col.find_cards("rated:1"))
     divisor = studied_card_count + due_card_count
@@ -25,9 +26,51 @@ def set_progress():
     bus.send(signal)
 
 
+def send_notification():
+    global app_name
+    global notification_id
+    global saved_due_card_count
+    due_card_count = len(mw.col.find_cards("is:new or is:due"))
+    to_notify = due_card_count > saved_due_card_count
+    saved_due_card_count = due_card_count
+    if not to_notify:
+        return
+    short_app_name = "Anki"
+    qt_notification_id = QtCore.QVariant(notification_id)
+    qt_notification_id.convert(QtCore.QVariant.UInt)
+    icon = app_name
+    title = "Anki"
+    text = str(due_card_count) + (" card" if due_card_count == 1 else " cards") + " to study"
+    actions_list = QtDBus.QDBusArgument([], QtCore.QMetaType.QStringList)
+    hint = {}
+    time = -1
+    item = "org.freedesktop.Notifications"
+    path = "/org/freedesktop/Notifications"
+    interface = "org.freedesktop.Notifications"
+    bus = QtDBus.QDBusConnection.sessionBus()
+    if not bus.isConnected():
+        print("Not connected to dbus!")
+    notify = QtDBus.QDBusInterface(item, path, interface, bus)
+    if notify.isValid():
+        msg = notify.call(QtDBus.QDBus.AutoDetect, "Notify", short_app_name,
+                          qt_notification_id, icon, title, text,
+                          actions_list, hint, time)
+        if msg.errorName():
+            print("Failed to send notification!")
+            print(msg.errorMessage())
+        notification_id = msg.arguments()[0]
+    else:
+        print("Invalid dbus interface!")
+
+
 config = mw.addonManager.getConfig(__name__)
-app_name = config['app_name']
+app_name = config['app_name']  # application name is not always net.ankiweb.Anki
+notification_id = 0  # we want to store notification ID to replace it if it is not closed
+saved_due_card_count = 0  # we want to send notifications only if the number of due cards changes
 gui_hooks.collection_did_load.append(lambda _: set_progress())
 gui_hooks.reviewer_did_show_question.append(lambda _: set_progress())
 gui_hooks.reviewer_will_end.append(set_progress)
 gui_hooks.sync_did_finish.append(set_progress)
+mw.progress.timer(60000, set_progress, True)
+gui_hooks.sync_did_finish.append(send_notification)
+mw.progress.timer(60000, send_notification, True)
